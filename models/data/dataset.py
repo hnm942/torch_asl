3c14bcf7528d65c670735c4a4005cb036d5e9b96
hnm942 <huy.nm170773@gmail.com>
2023-07-10 23:12:55 +0700

    add get landmark to preprocessing

--- models/data/dataset.py
+++ models/data/dataset.py
@@ -2,15 +2,16 @@ import numpy as np
 import pandas as pd
 import torch
 from torch.utils import data
-
+from models.utils.config import ASLConfig
+from models.data import preprocess, augmentation
 class AslDataset(data.Dataset):
-    def __init__(self, df, cfg, phase):
+    def __init__(self, df, npy_path, cfg, phase = "train"):
         self.df = df
-        self.max_len = cfg.config.max_position_embeddings  - 1
+        self.max_len = cfg.max_position_embeddings  - 1
         self.phase = phase
-        self.dis_idx0, self.dis_idx1 = torch.where(torch.triu(torch.ones((21, 21)), 1) == 1)
-        self.dis_idx2, self.dis_idx3 = torch.where(torch.triu(torch.ones((20, 20)), 1) == 1)
-
+        #load npy:
+        print("load data in: {}".format(npy_path))
+        self.npy = np.load(npy_path)
     def normalize(self, data):
         ref = data.flatten()
         ref = data[~data.isnan()]
@@ -18,15 +19,27 @@ class AslDataset(data.Dataset):
         return (data - mu) / std
     
     def get_landmarks(self, data):
-        pass
-
+        landmarks = self.npy[data.idx:data.idx + data.length]
+        # augumentation if training
+        if self.phase == "train":
+            # random interpolation
+            landmarks = augmentation.aug2(landmarks)
+            
+            
     def __len__(self):
         return len(self.df)
     
     def __getitem__(self, indices):
-        data = self.df.iloc[indices]
-        return super().__getitem__(indices)
+        data = self.df.iloc[indices] # row in file csv
+        landmark = self.get_landmarks(data)
+        
+        
+        return {}
     
-
-
-
+config = ASLConfig(max_position_embeddings= 90)
+# create df in numpy
+npy_path = "/workspace/data/asl_numpy_dataset/train_landmarks/train_npy.npy"
+df = pd.read_csv("/workspace/data/asl_numpy_dataset/train.csv")
+asl_dataset = AslDataset(df, npy_path, config)
+asl_dataset.__getitem__(0)
+# print(asl_dataset.__len__())
--- tools/combine_npy.py
+++ tools/combine_npy.py
@@ -7,17 +7,13 @@ if __name__ == "__main__":
     data_dir = "/workspace/data/asl_numpy_dataset"
     npyparquet_dir= os.path.join(data_dir, "train_landmarks")
     npyparquet_list = os.listdir(npyparquet_dir)
-    csv_dir = "/workspace/data/asl-fingerspelling/train.csv"
+    csv_dir = os.path.join(data_dir, "train.csv")
     df = pd.read_csv(csv_dir)
-    # for npyparquet in npyparquet_list:
-    #     parquet_path = os.path.join(npyparquet_dir, npyparquet)
-    #     file_id = npyparquet.split["."][0]
-    # list to check if had save npy
     file_list = df["file_id"].unique()
     new_df = pd.DataFrame(columns=(df.columns.append(pd.Index(["length"]))))
-    i = 0
-    # print(new_df.columns)
-    for file in file_list:
+    start_array = True
+    for i in range(65, len(file_list)):
+        file = file_list[i]
         # get npy landmark
         file_df = df[df["file_id"] == file]
         file_id = "{}.parquet.npy".format(file)
@@ -25,22 +21,15 @@ if __name__ == "__main__":
         landmarks = np.load(file_path)
         # get sequences
         sequences_list = file_df["sequence_id"].unique()    
-        print("{}_{}".format(file, len(sequences_list)))
+        print("{}_{}_{}".format(i, file, len(sequences_list)))
         for sequence_id in sequences_list:
             row = file_df[file_df["sequence_id"] == sequence_id].iloc[0]
             new_landmarks = landmarks[landmarks[ :, 0] == sequence_id]
-            new_row = {"path": row["path"], "file_id": row["file_id"], "sequence_id": row["sequence_id"], 
-                                    "participant_id": row["participant_id"], "phrase": row["phrase"], "length": new_landmarks.shape[0]}
-            new_df = pd.concat([new_df, pd.DataFrame([new_row])], ignore_index=True)
-            # print(row["file_id"])
-
-            # if i == 0:
-            #     arr = new_landmarks
-            #     i = 1
-            # else:
-            #     arr = np.concatenate((arr, new_landmarks), axis = 0)
-            #     pass
-    new_df.to_csv("/workspace/data/asl_numpy_dataset/train.csv")    
-    # np.save(os.path.join(data_dir, "train_npy.npy"), arr)
-
-        
+            if  start_array:
+                arr = new_landmarks
+                start_array = False
+            else:
+                arr = np.concatenate((arr, new_landmarks), axis = 0)
+        if i % 5 == 4 or i == len(file_list) - 1:
+            np.save(os.path.join(npyparquet_dir, "temp", f"{i}.npy"), arr)
+            start_array = True
